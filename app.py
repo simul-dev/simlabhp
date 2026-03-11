27fe6384ee40cfc1ba7f2aef28f7570b0870f943
simul-dev <eugene4213@naver.com>
2026-03-11 17:08:28 +0900

    remove mongodb

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-from flask import Flask, render_template, request, jsonify
-import requests
-from bs4 import BeautifulSoup
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.ggixz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-db = client.test
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def home():
@@ -17,40 +9,6 @@ def home():
 @app.route('/<html_name>')
 def html(html_name):
     return render_template(html_name)
-#
-# @app.route("/movie", methods=["POST"])
-# def movie_post():
-#     url_receive = request.form['url_give']
-#     star_receive = request.form['star_give']
-#     comment_receive = request.form['comment_give']
-#
-#     headers = {
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-#     data = requests.get(url_receive, headers=headers)
-#
-#     soup = BeautifulSoup(data.text, 'html.parser')
-#
-#     title = soup.select_one('meta[property="og:title"]')['content']
-#     image = soup.select_one('meta[property="og:image"]')['content']
-#     desc = soup.select_one('meta[property="og:description"]')['content']
-#
-#     doc = {
-#         'title':title,
-#         'image':image,
-#         'desc':desc,
-#         'star':star_receive,
-#         'comment':comment_receive
-#     }
-#     db.movies.insert_one(doc)
-#
-#     return jsonify({'msg': 'posting complete'})
-#
-#
-# @app.route("/movie", methods=["GET"])
-# def movie_get():
-#     movie_list = list(db.movies.find({}, {'_id': False}))
-#     return jsonify({'movies':movie_list})
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
